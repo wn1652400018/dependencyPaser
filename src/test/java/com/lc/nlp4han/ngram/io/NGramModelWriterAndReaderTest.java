73ba275be09bdf906039045149478051bd6f60fb
kidden <kiddenliu@sina.com>
2017-11-18 14:52:27 +0800

    初次提交n元语法语言模型包ngram。

---
+++ src/test/java/com/lc/nlp4han/ngram/io/NGramModelWriterAndReaderTest.java
@@ -0,0 +1,132 @@
+package com.lc.nlp4han.ngram.io;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.lc.nlp4han.ngram.model.AbstractLanguageModelTrainer;
+import com.lc.nlp4han.ngram.model.MLLanguageModelTrainer;
+import com.lc.nlp4han.ngram.model.NGramCounter;
+import com.lc.nlp4han.ngram.model.NGramLanguageModel;
+import com.lc.nlp4han.ngram.utils.Gram;
+import com.lc.nlp4han.ngram.utils.StringGram;
+
+
+/**
+ *<ul>
+ *<li>Description: 测试模型的读写 
+ *<li>Company: HUST
+ *<li>@author Sonly
+ *<li>Date: 2017年7月31日
+ *</ul>
+ */
+public class NGramModelWriterAndReaderTest {
+
+	Gram[] grams;
+	String smooth = "ml";
+	AbstractLanguageModelTrainer modelTrainer;
+	NGramLanguageModel trainModel;
+	NGramLanguageModel loadModel;
+	
+	@Before
+	public void setup() {
+		grams = new Gram[]{new StringGram("1"), new StringGram("2"),new StringGram("3"), new StringGram("4"),
+				  		   new StringGram("3"), new StringGram("2"),new StringGram("3"), new StringGram("5")};
+		NGramCounter nGramCounter = new NGramCounter(grams, 3);
+		modelTrainer = new MLLanguageModelTrainer(nGramCounter, 3);
+		
+		trainModel = modelTrainer.trainModel();
+	}
+	
+	/**
+	 * 测试arpa格式n元模型文本文件的读写  
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	@Test
+	public void testARPATextFileNGramModleWriterAndReader() throws IOException, ClassNotFoundException {
+		//写入模型
+		ARPATextFileNGramModleWriter arpaNGramModelWiter = new ARPATextFileNGramModleWriter(trainModel, 
+				new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_arpa_trigram.lm"));
+		arpaNGramModelWiter.persist();
+		
+		//读取模型
+		File file = new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_arpa_trigram.lm");
+		ARPATextFileNGramModleReader reader = new ARPATextFileNGramModleReader(file);
+		loadModel = reader.constructModel();
+		
+		//判断模型
+		assertEquals(trainModel, loadModel);
+	}
+	
+	/**
+	 * 测试n元模型文本文件的读写  
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	@Test
+	public void testTextFileNGramModleWriterAndReader() throws IOException, ClassNotFoundException {
+		//写入模型
+		TextFileNGramModelWriter tFNGramModelWiter = new TextFileNGramModelWriter(trainModel, 
+				new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_trigramModel.txt"));
+		tFNGramModelWiter.persist();
+		
+		//读取模型
+		File file = new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_trigramModel.txt");
+		TextFileNGramModelReader reader = new TextFileNGramModelReader(file);
+		loadModel = reader.constructModel();
+		
+		//判断模型
+		assertEquals(trainModel, loadModel);
+	}
+	
+	/**
+	 * 测试n元模型序列化文件的读写
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	@Test
+	public void testObjectFileNGramModleWriterAndReader() throws IOException, ClassNotFoundException {
+		//写入模型
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_objectModel.out")));
+		ObjectFileNGramModelWriter oFNGramModelWriter = new ObjectFileNGramModelWriter(trainModel, oos);
+		oFNGramModelWriter.persist();
+				
+		//读取模型
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_objectModel.out")));
+		ObjectFileNGramModelReader reader = new ObjectFileNGramModelReader(ois);
+		loadModel = reader.constructModel();
+				
+		//判断模型
+		assertEquals(trainModel, loadModel);
+	}
+	
+	/**
+	 * 测试n元模型二进制文件的读写
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	@Test
+	public void testBinaryFileNGramModleWriterAndReader() throws IOException, ClassNotFoundException {
+		//写入模型
+		BinaryFileNGramModelWriter bFNGramModelWiter = new BinaryFileNGramModelWriter(trainModel, 
+				new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_binaryModel.bin"));
+		bFNGramModelWiter.persist();
+		
+		//读取模型
+		File file = new File("src\\test\\java\\com\\lc\\nlp4han\\ngram\\io\\TestIO_binaryModel.bin");
+		BinaryFileNGramModelReader reader = new BinaryFileNGramModelReader(file);
+		loadModel = reader.constructModel();
+		
+		//判断模型
+		assertEquals(trainModel, loadModel);
+	}	
+}
