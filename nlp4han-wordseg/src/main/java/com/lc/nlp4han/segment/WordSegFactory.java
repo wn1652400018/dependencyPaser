a68b9a93e5cac2ff59ad9a27a9c4b64ab4945285
kidden <kiddenliu@sina.com>
2018-04-18 20:10:20 +0800

    优化工厂类，使用单件模式只创建一次中文分词器。

--- nlp4han-wordseg/src/main/java/com/lc/nlp4han/segment/WordSegFactory.java
+++ nlp4han-wordseg/src/main/java/com/lc/nlp4han/segment/WordSegFactory.java
@@ -7,22 +7,38 @@ import com.lc.nlp4han.ml.util.ModelWrapper;
 import com.lc.nlp4han.segment.maxent.WordSegContextGeneratorConf;
 import com.lc.nlp4han.segment.maxent.WordSegmenterME;
 
+/**
+ * 中文分词器工厂类
+ * 
+ * @author 刘小峰
+ *
+ */
 public class WordSegFactory
 {
+	private static WordSegmenter wordSegmenter;
+	
 	private WordSegFactory()
 	{
 	}
 
+	/**
+	 * 装入中文分词模型，并生成中文分词器
+	 * @return 中文分词器
+	 * @throws IOException
+	 */
 	public static WordSegmenter getWordSegmenter() throws IOException
 	{
+		if(wordSegmenter != null)
+			return wordSegmenter;
+		
 		String modelName = "com/lc/nlp4han/segment/ws-pku-gb.model";
 
 		InputStream modelIn = WordSegFactory.class.getClassLoader().getResourceAsStream(modelName);
 		ModelWrapper model = new ModelWrapper(modelIn);
 
-		WordSegmenterME segmenter = new WordSegmenterME(model, new WordSegContextGeneratorConf());
+		wordSegmenter = new WordSegmenterME(model, new WordSegContextGeneratorConf());
 
-		return segmenter;
+		return wordSegmenter;
 	}
 
 	public static void main(String[] args) throws IOException
