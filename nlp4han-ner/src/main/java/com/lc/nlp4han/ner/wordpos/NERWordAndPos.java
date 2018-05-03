2efdf4e7358881ad4c40c4a3a9ffdec4ab8cb4f4
kidden <kiddenliu@sina.com>
2018-05-03 15:39:11 +0800

    注释方法。

--- nlp4han-ner/src/main/java/com/lc/nlp4han/ner/wordpos/NERWordAndPos.java
+++ nlp4han-ner/src/main/java/com/lc/nlp4han/ner/wordpos/NERWordAndPos.java
@@ -5,6 +5,7 @@ import com.lc.nlp4han.ner.NamedEntity;
 /**
  * 基于词性标注的命名实体识别
  * 
+ * @author 刘小峰
  * @author 王馨苇
  *
  */
@@ -35,7 +36,7 @@ public interface NERWordAndPos
 	 * 读入一句词性标注的语料，得到指定的命名实体
 	 * 
 	 * @param sentence
-	 *            读取的词性标注的语料
+	 *            读取的词性标注的语料，词和词性间/分割
 	 * @param flag
 	 *            命名实体标记
 	 * @return
@@ -59,7 +60,7 @@ public interface NERWordAndPos
 	 * 读入一句词性标注的语料，得到最好的K个结果
 	 * 
 	 * @param sentence
-	 *            读取的词性标注的语料
+	 *            读取的词性标注的语料，词和词性间/分割
 	 * @return
 	 */
 	public NamedEntity[][] ner(String sentence, int k);
