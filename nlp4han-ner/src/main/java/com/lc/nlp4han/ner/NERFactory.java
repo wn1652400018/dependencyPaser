85ee4171c420b7b46a324b5bf5c636888366b635
kidden <kiddenliu@sina.com>
2018-05-08 18:43:15 +0800

    增加NERFactory类，修改NamedEntity的toString方法。

---
+++ nlp4han-ner/src/main/java/com/lc/nlp4han/ner/NERFactory.java
@@ -0,0 +1,51 @@
+package com.lc.nlp4han.ner;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.lc.nlp4han.ml.util.ModelWrapper;
+import com.lc.nlp4han.ner.word.NERWord;
+import com.lc.nlp4han.ner.word.NERWordContextGeneratorConf;
+import com.lc.nlp4han.ner.word.NERWordME;
+
+public class NERFactory
+{
+
+	private static NERWord ner;
+
+	private NERFactory()
+	{
+	}
+
+	/**
+	 * 装入中文NER模型，并生成NER标注器
+	 * 
+	 * @return 中文NER
+	 * @throws IOException
+	 */
+	public static NERWord getPOSTagger() throws IOException
+	{
+		if (ner != null)
+			return ner;
+
+		String modelName = "com/lc/nlp4han/ner/ner-pd-word-gb.model";
+
+		InputStream modelIn = NERFactory.class.getClassLoader().getResourceAsStream(modelName);
+		ModelWrapper model = new ModelWrapper(modelIn);
+
+		ner = new NERWordME(model, new NERWordContextGeneratorConf());
+
+		return ner;
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		String words = "李 鹏 在 北京 视察 。";
+
+		NERWord tagger = NERFactory.getPOSTagger();
+		NamedEntity[] tags = tagger.ner(words);
+		for(int i=0; i<tags.length; i++)
+			System.out.println(tags[i]);
+	}
+
+}
--- nlp4han-ner/src/main/java/com/lc/nlp4han/ner/NamedEntity.java
+++ nlp4han-ner/src/main/java/com/lc/nlp4han/ner/NamedEntity.java
@@ -1,12 +1,15 @@
 package com.lc.nlp4han.ner;
 
+import java.util.Arrays;
+
 /**
  * 命名实体类
  * 
  * @author 王馨苇
  *
  */
-public class NamedEntity {
+public class NamedEntity
+{
 
 	private String type;
 	private String string;
@@ -14,68 +17,84 @@ public class NamedEntity {
 	private int end;
 	private String[] words;
 
-	public void setType(String type){
+	public void setType(String type)
+	{
 		this.type = type;
 	}
-	
-	public void setString(String string){
+
+	public void setString(String string)
+	{
 		this.string = string;
 	}
-	
-	public void setStart(int start){
+
+	public void setStart(int start)
+	{
 		this.start = start;
 	}
-	
-	public void setEnd(int end){
+
+	public void setEnd(int end)
+	{
 		this.end = end;
 	}
-	
-	public void setWords(String[] words){
+
+	public void setWords(String[] words)
+	{
 		this.words = words;
 	}
-	
+
 	/**
 	 * 实体的类型
+	 * 
 	 * @return
 	 */
-	public String getType(){
+	public String getType()
+	{
 		return this.type;
 	}
-	
+
 	/**
 	 * 实体字符串
+	 * 
 	 * @return
 	 */
-	public String getString(){
+	public String getString()
+	{
 		return this.string;
 	}
-	
+
 	/**
 	 * 实体起始位置，词或字位置
+	 * 
 	 * @return
 	 */
-	public int getStart(){
+	public int getStart()
+	{
 		return this.start;
 	}
-	
+
 	/**
 	 * 实体结束位置，词或字位置
+	 * 
 	 * @return
 	 */
-	public int getEnd(){
+	public int getEnd()
+	{
 		return this.end;
 	}
-	
+
 	/**
 	 * 实体构成的词序列
+	 * 
 	 * @return
 	 */
-	public String[] getWords(){
+	public String[] getWords()
+	{
 		return this.words;
 	}
 
 	@Override
-	public String toString() {
-		return "["+this.string+"]"+this.type+"("+this.start+","+this.end+")"+"{"+this.words+"}";
+	public String toString()
+	{
+		return "[" + this.string + "]" + this.type + "(" + this.start + "," + this.end + ")" + "{" + Arrays.toString(this.words) + "}";
 	}
 }
