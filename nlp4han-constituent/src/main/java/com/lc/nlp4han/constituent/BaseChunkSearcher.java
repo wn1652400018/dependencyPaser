973d3ee33beefefe2514753372a9084b8b8c75e8
lincham <2627251823@qq.com>
2018-07-03 11:32:52 +0800

    1、修改部分问题；

--- nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/BaseChunkSearcher.java
+++ nlp4han-constituent/src/main/java/com/lc/nlp4han/constituent/BaseChunkSearcher.java
@@ -2,9 +2,7 @@ package com.lc.nlp4han.constituent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
-import org.omg.IOP.TAG_CODE_SETS;
 
 import com.lc.nlp4han.constituent.TreeNode;
 
