779ef22003bdfeb5d429c8c4c316faf3311a12ad
hp <hp@hp-PC>
2018-08-02 07:14:11 +0800

    修改simpleValidator

--- nlp4han-dep/src/main/java/com/lc/nlp4han/dependency/tb/SimpleValidator.java
+++ nlp4han-dep/src/main/java/com/lc/nlp4han/dependency/tb/SimpleValidator.java
@@ -23,6 +23,8 @@ public class SimpleValidator
 			{// 确保一个单词的中心词只能有一个
 				if (conf.getStack().peek().getIndexOfWord() == 0)
 					return false;
+				if (conf.getWordsBuffer().size() == 0)
+					return false;
 				for (Arc arc : conf.getArcs())
 				{
 					if (arc.getDependent() == conf.getStack().peek())
@@ -34,12 +36,53 @@ public class SimpleValidator
 				return true;
 			}
 
+			if (preAct.getBaseAction().equals("RIGHTARC_SHIFT"))
+			{
+				if (conf.getWordsBuffer().isEmpty())
+				{
+					System.out.println("因为buffer位空，故不能有RIGHTARC_SHIFT操作");
+					return false;
+				}
+				if (preAct.getRelation().equals("核心成分"))// 分类中只有rightarc才有可能是核心成分
+				{// 确保“核心”只能作为一个词语的中心词
+					if (conf.getStack().peek().getIndexOfWord() != 0)
+					{
+						return false;
+					}
+					for (Arc arc : conf.getArcs())
+					{
+						if (arc.getRelation().equals("核心成分"))
+							return false;
+					}
+					return true;
+				}
+				else
+				{
+					return true;
+				}
+			}
+
+			if (preAct.getBaseAction().equals("SHIFT"))
+			{
+				if (conf.getWordsBuffer().isEmpty())
+				{
+					System.out.println("因为buffer位空，故不能有SHIFT操作");
+					return false;
+				}
+				else
+					return true;
+			}
+
 			if (preAct.getBaseAction().equals("REDUCE"))
 			{
+				if (conf.getStack().peek().getIndexOfWord() != 0 && conf.getWordsBuffer().size() == 0)
+					return true;
 				if (conf.getStack().peek().getIndexOfWord() == 0)
+				{
+					System.out.println("因为栈顶是ROOT，故不能有REDUCE操作");
+					return false;
+				}
 
-					if (conf.getWordsBuffer().size() == 0)
-						return true;
 				for (Arc arc : conf.getArcs())
 				{
 					if (arc.getHead().getIndexOfWord() == conf.getStack().peek().getIndexOfWord()
@@ -50,21 +93,6 @@ public class SimpleValidator
 				}
 				System.out.println("因为栈顶单词还没有建立依存关系，故不能有REDUCE操作");
 				return false;
-			}
-
-			if (preAct.getRelation().equals("核心成分"))
-			{// 确保“核心”只能作为一个词语的中心词
-
-				for (Arc arc : conf.getArcs())
-				{
-					if (arc.getRelation().equals("核心成分"))
-						return false;
-				}
-				return true;
-			}
-			else
-			{
-				return true;
 			}
 
 		}
